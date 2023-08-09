dec714000a55e85978aae901cf61383d02e23968
rodolll <31144940+rodolll@users.noreply.github.com>
2023-08-09 11:03:25 -0300

    comentarioenlaclasejemplo
    agregue un comentario

--- src/ejemplo2/Ejemplo2.java
+++ src/ejemplo2/Ejemplo2.java
@@ -16,6 +16,9 @@ public class Ejemplo2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    /*    "hola que haces"
+     * 
+    */
     }
     
 }
